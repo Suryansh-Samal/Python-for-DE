14d84d9d7d61e71d3cf0e114a133a3d12d5a6124
Suryansh Samal <suryanshsamal4@gmail.com>
2026-07-31 22:12:25 +0530

    Added f string

---
+++ 03_Strings/04_f_strings.py
@@ -0,0 +1,13 @@
+"""
+Topic: f-Strings
+Description: Demonstrates string formatting using Python f-strings.
+Author: Suryansh
+"""
+
+# ============================================
+# f-String Example
+# ============================================
+
+name = "Suryansh"
+
+print(f"Hello {name}, How are you?")
